c056fa23ccf8f2221e08725ce05049f3ef624766
praladsapkota-08 <okmqax@gmail.com>
2026-01-01 21:17:58 +0545

    app.py updated

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.llms import Ollama
+from langchain_community.llms import Ollama # type: ignore
 
 import streamlit as st
 import os
